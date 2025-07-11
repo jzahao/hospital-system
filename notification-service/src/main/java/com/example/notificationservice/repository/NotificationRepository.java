be3bff86cd86ec37f17d606d49782f446e07ffc4
Gia Hao <120190345+jzahao@users.noreply.github.com>
2025-07-11 23:03:43 +0700

    Delete notification-service directory

--- notification-service/src/main/java/com/example/notificationservice/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.notificationservice.repository;
-
-import com.example.notificationservice.model.Notification;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface NotificationRepository extends MongoRepository<Notification, String> {
-}
