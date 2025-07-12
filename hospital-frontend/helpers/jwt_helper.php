1bb182bb6b82f749876d71de70b6db70285bfb23
Gia Hao <120190345+jzahao@users.noreply.github.com>
2025-07-12 17:17:45 +0700

    Delete hospital-frontend directory

--- hospital-frontend/helpers/jwt_helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-function getPayloadFromToken($token) {
-    $parts = explode('.', $token);
-    if (count($parts) !== 3) return null;
-    
-    $payload = base64_decode(strtr($parts[1], '-_', '+/'));
-    return json_decode($payload, true);
-}
